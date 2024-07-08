8efeadb477c8037f6da8d39dfc21e66cc6420fd2
mohankr7870 <mohan.kumar@shinedezign.com>
2024-07-08 18:34:54 +0530

    change the swall msg

--- resources/views/customer/edit_product.blade.php
+++ resources/views/customer/edit_product.blade.php
@@ -76,10 +76,10 @@
 
     Swal.fire({
         title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        text: "You won't delete the product!",
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#1B1B1B',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
