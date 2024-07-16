a13bf676f6b331ab7bbd225c2756ccb3ab9c128b
abhisheksharmashine <abhishek.singh@shinedezign.com>
2024-07-16 18:25:33 +0530

    changes

--- resources/views/auth/verify_otp.blade.php
+++ resources/views/auth/verify_otp.blade.php
@@ -1,52 +1,77 @@
 @extends('layouts.app')
 @section('content')
     <div class="cust-form-bg full-hight-sm">
-        <div class="form-setup login-form">
+        <div class="form-setup login-form otp-form">
             <x-alert />
-            @if (emailValidate(auth()->user()->id))
-                <form id="emailOtpVerify" class="form-inline emailverify" action="{{ route('verify.email.otp') }}"
-                    method="POST">
-                    @csrf
 
-                    <div class="form-group">
-                        <div class="formfield">
-                            <input id="emailotp" type="text"
-                                class="form-control form-class @error('emailotp') is-invalid @enderror" name="emailotp"
-                                value="{{ old('emailotp') }}" autocomplete="name" placeholder="Enter OTP">
-                        </div>
-                        @error('emailotp')
-                            <span class="invalid-feedback" role="alert">
-                                {{ $message }}
-                            </span>
-                        @enderror
+            <h3>You have verify email and phone OTP</h3>
+            {{-- @if (emailValidate(auth()->user()->id)) --}}
+            <form id="emailOtpVerify" class="form-inline emailverify" action="{{ route('verify.email.otp') }}" method="POST">
+                @csrf
+
+                <div class="form-group ">
+                    <label for="">Email OTP Send On: {{ auth()->user()->email }}</label>
+                    <div class="formfield custm-otp-field">
+                        <input id="emailotp" type="text"
+                            class="form-control form-class @error('emailotp') is-invalid @enderror" name="emailotp"
+                            value="{{ old('emailotp') }}" @if (auth()->user()->emailOtp->status == 1) disabled @endif
+                            autocomplete="name"
+                            placeholder="@if (auth()->user()->emailOtp->status == 1) verified @else Enter OTP @endif">
+                        <button type="submit" @if (auth()->user()->emailOtp->status == 1) disabled @endif
+                            class="button primary-btn full-btn">
+                            @if (auth()->user()->emailOtp->status != 1)
+                                {{ __('Verify OTP') }}
+                            @else
+                                <i class="fa-solid fa-circle-check"></i>
+                            @endif
+                        </button>
                     </div>
-                    <button type="submit" class="button primary-btn full-btn">{{ __('Verify OTP') }}</button>
-                    <a href="{{ route('resend.otp', ['type' => 'email']) }}" class="text-dark">Resend</a>
-                </form>
-            @endif
-            <hr>
-            @if (phoneNumberValidate(auth()->user()->id))
-                <form id="phoneOtpVerify" class="form-inline phoneverify" action="{{ route('verify.phone.otp') }}"
-                    method="POST">
-                    @csrf
-                    <div class="form-group">
-                        <div class="formfield">
-                            <input id="phoneotp" type="text"
-                                class="form-control form-class @error('phoneotp') is-invalid @enderror" name="phoneotp"
-                                value="{{ old('phoneotp') }}" autocomplete="name" placeholder="Enter OTP">
-                        </div>
-                        @error('phoneotp')
-                            <span class="invalid-feedback" role="alert">
-                                {{ $message }}
-                            </span>
-                        @enderror
+                    @error('emailotp')
+                        <span class="invalid-feedback" role="alert">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+                <div class="d-flex justify-content-end">
+                    <a href="{{ route('resend.otp', ['type' => 'email']) }}" class="text-dark ">Resend
+                        Email OTP</a>
+                </div>
+            </form>
+            {{-- @endif --}}
+
+            {{-- @if (phoneNumberValidate(auth()->user()->id)) --}}
+            <form id="phoneOtpVerify" class="form-inline phoneverify" action="{{ route('verify.phone.otp') }}"
+                method="POST">
+                @csrf
+                <div class="form-group mt-3">
+                    <label for="">Phone OTP Send On: {{ auth()->user()->phone_number }}</label>
+                    <div class="formfield custm-otp-field   ">
+                        <input id="phoneotp" type="text"
+                            class="form-control form-class @error('phoneotp') is-invalid @enderror" name="phoneotp"
+                            value="{{ old('phoneotp') }}" @if (auth()->user()->phoneOtp->status == 1) disabled @endif
+                            autocomplete="name"
+                            placeholder="@if (auth()->user()->phoneOtp->status == 1) verified @else Enter OTP @endif">
+                        <button type="submit" id="submitPhoneOtp" @if (auth()->user()->phoneOtp->status == 1) disabled @endif
+                            class="button primary-btn full-btn">
+                            @if (auth()->user()->phoneOtp->status != 1)
+                                {{ __('Verify OTP') }}
+                            @else
+                                <i class="fa-solid fa-circle-check"></i>
+                            @endif
+                        </button>
                     </div>
-                    {{-- @dd($user) --}}
-                    <button type="submit" id="submitPhoneOtp"
-                        class="button primary-btn full-btn">{{ __('Verify OTP') }}</button>
-                    <a href="{{ route('resend.otp', ['type' => 'phone_number']) }}" class="text-dark">Resend</a>
-                </form>
-            @endif
+                    @error('phoneotp')
+                        <span class="invalid-feedback" role="alert">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+                {{-- @dd($user) --}}
+                <div class="d-flex justify-content-end">
+                    <a href="{{ route('resend.otp', ['type' => 'phone_number']) }}" class="text-dark">Resend Phone OTP</a>
+                </div>
+            </form>
+            {{-- @endif --}}
         </div>
     </div>
 @endsection
@@ -57,18 +82,18 @@
                 e.preventDefault();
                 var url = $(this).attr('action');
                 var formData = new FormData($(this)[0]);
-                console.log(url, "herere");
                 response = ajaxCall(url, 'post', formData)
                 response.then(response => {
                     console.log(response.login);
                     if (response.login == 1) {
                         window.location.href = response.url;
                     } else if (response.status == true) {
-                        $('.emailverify').addClass('d-none');
-                        return iziToast.success({
-                            message: response.message,
-                            position: 'topRight'
-                        });
+                        // $('#emailotp').prop('disabled', true);
+                        location.reload();
+                        // return iziToast.success({
+                        //     message: response.message,
+                        //     position: 'topRight'
+                        // });
                     }
 
                 })
@@ -81,15 +106,15 @@
                 var formData = new FormData($(this)[0]);
                 response = ajaxCall(url, 'post', formData)
                 response.then(response => {
-                    console.log(response.login);
                     if (response.login == 1) {
                         window.location.href = response.url;
                     } else if (response.status == true) {
-                        $('.phoneverify').addClass('d-none');
-                        return iziToast.success({
-                            message: response.message,
-                            position: 'topRight'
-                        });
+                        location.reload()
+                        //$('#phoneotp').prop('disabled', true);
+                        // return iziToast.success({
+                        //     message: response.message,
+                        //     position: 'topRight'
+                        // });
                     }
 
                 })
