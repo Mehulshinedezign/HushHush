a7a34593316f5b2da7d20d816f8ef9b8d16878ba
Mehulshinedezign <149387652+Mehulshinedezign@users.noreply.github.com>
2024-09-19 17:44:49 +0530

    stripe indentity integration

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -131,9 +131,9 @@
                         </ul>
                     </div>
                     <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                                <span class="navbar-toggler-icon"></span>
-                                            </button> -->
+                                                                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                                                    <span class="navbar-toggler-icon"></span>
+                                                                </button> -->
                 </div>
             @endauth
             @guest
