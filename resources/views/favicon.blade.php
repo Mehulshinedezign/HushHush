a97cb4fbf949f02494bfc650bc7241d87e8853e9
gurjitnandra <gurjit.singh@shinedezign.com>
2024-10-10 15:50:07 +0530

    fixes

--- resources/views/favicon.blade.php
+++ resources/views/favicon.blade.php
@@ -1,4 +1,4 @@
-<link rel="apple-touch-icon" sizes="180x180" href="{{asset('favIcon/apple-touch-icon.png')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{asset('favIcon_nudora/apple-touch-icon.png')}}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favIcon_nudora\favicon-32x32.png')}}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favIcon_nudora\favicon-16x16.png')}}">
 <link rel="manifest" href="{{asset('favIcon_nudora/site.webmanifest')}}">
